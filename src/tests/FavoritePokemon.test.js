fb8dee8e161790afd3b881081adb87b1ce287f3d
Joao Peixoto <joao.peixoto678@gmail.com>
2022-11-23 12:45:38 -0300

    Virus 3 Aniquilado

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('Testa o componente <FavoritePokemon.js />', () => {
+  test('Testa se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const notFoundElement = screen.getByText(/no favorite pokémon found/i);
+    expect(notFoundElement).toBeInTheDocument();
+  });
+});
