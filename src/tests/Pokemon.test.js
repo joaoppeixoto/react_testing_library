b37d052ed2eb4e449f1f65419b01a21d57a5f12a
Joao Peixoto <joao.peixoto678@gmail.com>
2022-11-30 10:25:16 -0300

    Virus 6 Aniquilado

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,17 +9,18 @@ describe('Testa se é renderizado um card', () => {
   const pikachu = pokemonList[0];
   test('Testa as informações de determinado Pokémon', () => {
     renderWithRouter(<App />);
-    const { averageWeight: { value, measurementUnit } } = pikachu;
 
-    const name = screen.getByTestId('pokemon-name');
-    const type = screen.getByTestId('pokemon-type');
-    const weight = screen.getByTestId('pokemon-weight');
-    const img = screen.getByAltText(`${pikachu.name} sprite`).src;
+    const namePokemon = screen.getByTestId('pokemon-type');
+    const typePokemon = screen.getByTestId('pokemon-type');
+    const weightPokemon = screen.getByText('Average weight: 6.0 kg');
+    const imagePokemon = screen.getByAltText('Pikachu sprite');
+    const linkPokemon = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
 
-    expect(name.innerHTML).toBe(pikachu.name);
-    expect(type.innerHTML).toBe(pikachu.type);
-    expect(weight.innerHTML).toBe(`Average weight: ${value} ${measurementUnit}`);
-    expect(img).toBe(pikachu.image);
+
+    expect(namePokemon).toBeInTheDocument();
+    expect(typePokemon.textContent).toBe('Electric');
+    expect(weightPokemon).toBeInTheDocument();
+    expect(imagePokemon.src).toBe(linkPokemon);
   });
   test('Testa se o card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes deste Pokémon', () => {
     renderWithRouter(<App />);
@@ -38,9 +39,14 @@ describe('Testa se é renderizado um card', () => {
     userEvent.click(moreDetails);
     expect(history.location.pathname).toBe(`/pokemon/${pikachu.id}`);
   });
-  // test('Testa se a imagem de favorito possui o alt e src corretos', () => {
-  //   renderWithRouter(<App pokemonList={ pikachu } />);
-  //   const faviconAlt = screen.getByRole('img', { name: `${pikachu.name}/pikachu is marked as favorite/i` });
-  //   expect(faviconAlt).toContain('/star-icon.svg');
-  // });
+  test('Testa se a imagem de favorito possui o alt e src corretos', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(details);
+    const favIcon = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(favIcon);
+    const img = screen.getAllByRole('img')[1];
+    expect(img.src).toBe('http://localhost/star-icon.svg');
+    expect(img.alt).toBe('Pikachu is marked as favorite');
+  });
 });
