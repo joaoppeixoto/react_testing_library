378288ecb6076c7b044502ba932dcacb3aef3892
joao.peixoto678@gmail.com <joao.peixoto678@gmail.com>
2024-08-29 15:10:52 -0300

    Subindo para o git

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { queryByText, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+
+describe('Teste se as informações detalhadas do Pokémon selecionado são mostradas na tela', () => {
+  test('Testa se página contem um texto <name> Details, onde <name> é o nome do Pokémon', () => {
+    renderWithRouter(<App />);
+    const moreDetails = 'More details';
+    const pikachu = pokemonList[0];
+    userEvent.click(queryByText(moreDetails));
+    const details = screen.getByText(`${pikachu.name} Details`);
+    expect(details).toBeInTheDocument();
+  });
+});
